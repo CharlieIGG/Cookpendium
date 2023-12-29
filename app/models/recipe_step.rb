fd8391fb9699444c66498458b7340b35e278adb8
Carlos Garcia <charlie.igg@gmail.com>
2023-12-29 21:14:29 +0100

    adds relationships to RecipeStep

--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
+# Purpose: Model for recipe steps. A recipe step is a step in a recipe, such as "mix the flour and eggs together".
 class RecipeStep < ApplicationRecord
   belongs_to :recipe
+  has_many :recipe_step_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_step_ingredients
+
   translates :instruction
 end
