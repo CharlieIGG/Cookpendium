6f2b28e60e295baeb8f65c2f944c53165e792337
Carlos Garcia <charlie.igg@gmail.com>
2024-01-04 00:31:02 +0100

    recipe parser is now locale-sensitive

---
+++ app/services/ai_tools/recipe_parser_instructions.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module AITools
+  # contains the instructions necessary for the AI assistant to parse a recipe
+  class RecipeParserInstructions
+    def initialize(locale:)
+      @locale = locale
+    end
+
+    def sanity_filter_instructions
+      <<~HEREDOC
+        If the provided text is not a cooking recipe, please return the following JSON object:
+        {
+            "error": "The provided text is not a recipe. Please provide a recipe."
+        }
+      HEREDOC
+    end
+
+    def ingredient_structure_instructions
+      <<~HEREDOC
+        -- ingredient name (string). Do not include any units or quantities in the name.
+        -- quantity (number)
+        -- unit (string), as standard as possible, such as 'grams', 'cups', 'pinches', 'tablespoons', 'teaspoons', 'pounds', 'ounces', etc.
+      HEREDOC
+    end
+
+    def recipe_step_structure_instructions
+      <<~HEREDOC
+        -- a description (string): the detailed description on how to complete the instruction (e.g. "Preheat the oven to 350 degrees Fahrenheit")
+        -- an instruction (string): a short sentence thath summarizes the step in 45 characters or less (e.g. "Preheat the oven")
+        --- Note: the "instruction" is a supposed to be a shortened version of the "description" that can be used as a title for the step.
+        -- a step_number (integer), indicating the order of the step
+        -- a list of ingredients, as an array of objects, with the same structure as the main ingredients array.
+      HEREDOC
+    end
+
+    def recipe_step_splitting_instructions
+      <<~HEREDOC
+        If a recipe step contains multiple actions in any part of the original text, please split them into separate recipe_steps.
+        Typically this is identified by the word "and" or a comma between two actions (verbs).
+        Also if instructions are given in a paragraph separated by periods, please split them into separate recipe_steps.
+      HEREDOC
+    end
+
+    def locale_detection_instructions
+      <<~HEREDOC
+        Please ensure that you return the recipe in #{I18n.t("languages.#{@locale}")} \
+        regardless of the language the recipe was written in. This means you might \
+        have to translate the recipe into #{I18n.t("languages.#{@locale}")}.
+        Ensure you translate the title, the ingredients, and the recipe steps.
+      HEREDOC
+    end
+
+    def recipe_structure_instructions
+      <<~HEREDOC.freeze
+        Take the following text and parse it into a JSON recipe object.
+        The recipe object should have the following attributes:
+        - title
+        - description
+        - ingredients, as an array of objects, each including:
+        #{ingredient_structure_instructions}
+        - recipe_steps, as an array of objects, each containing:
+        #{recipe_step_structure_instructions}
+
+        #{recipe_step_splitting_instructions}
+      HEREDOC
+    end
+  end
+end
--- spec/services/ai_tools/recipe_parser_spec.rb
+++ spec/services/ai_tools/recipe_parser_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe AITools::RecipeParser, type: :service do
-  describe '#parse' do
+RSpec.describe AITools::RecipeParser, type: :service do # rubocop:disable Metrics/BlockLength
+  describe '#parse' do # rubocop:disable Metrics/BlockLength
     context 'when a proper text is entered' do
       it 'returns a valid JSON representing a recipe' do
         VCR.use_cassette('recipe_parser/valid_recipe') do
@@ -9,7 +9,7 @@ RSpec.describe AITools::RecipeParser, type: :service do
           parser = AITools::RecipeParser.new(input_text)
           result = parser.parse
           expect(result).to be_a(Hash)
-          expect(result).to include('title', 'description', 'ingredients', 'recipe_steps')
+          expect(result).to include('title', 'description', 'ingredients', 'recipe_steps', 'locale')
         end
       end
     end
@@ -22,6 +22,19 @@ RSpec.describe AITools::RecipeParser, type: :service do
           result = parser.parse
           expect(result).to be_a(Hash)
           expect(result).to include('error')
+        end
+      end
+    end
+
+    context 'returning the recipe in the locale the request was initiated from' do
+      it 'returns a JSON with the recipe in the locale the request was initiated from' do
+        VCR.use_cassette('recipe_parser/valid_recipe_in_german') do
+          input_text = File.read(Rails.root.join('spec', 'fixtures', 'valid_raw_recipe_german.txt'))
+          parser = AITools::RecipeParser.new(input_text, locale: 'en')
+          result = parser.parse
+          expect(result).to be_a(Hash)
+          expect(result).to include('title', 'description', 'ingredients', 'recipe_steps', 'locale')
+          expect(result['title']).to eq('Potato Salad')
         end
       end
     end
