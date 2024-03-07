d1b90580af5e020d8a6d365e2d8accd0bf359072
Carlos Garcia <charlie.igg@gmail.com>
2024-03-07 23:24:08 +0100

    specs passing... need to refactor

--- spec/system/recipes/searching_recipes_spec.rb
+++ spec/system/recipes/searching_recipes_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Searching Recipes', type: :system do
-  let!(:recipe1) { create(:recipe, title: 'Spaghetti Bolognese', description: 'Classic Italian dish') }
-  let!(:recipe2) { create(:recipe, title: 'Chicken Curry', description: 'Delicious Indian curry') }
+  let!(:recipe1) { create(:recipe, :with_steps, title: 'Spaghetti Bolognese', description: 'Classic Italian dish') }
+  let!(:recipe2) { create(:recipe, :with_steps, title: 'Chicken Curry', description: 'Delicious Indian curry') }
   let!(:ingredient0) { create(:ingredient, name: 'salt') }
   let!(:ingredient1) { create(:ingredient, name: 'spaghetti') }
   let!(:ingredient2) { create(:ingredient, name: 'tomato sauce') }
@@ -18,38 +18,53 @@ RSpec.describe 'Searching Recipes', type: :system do
   it 'searches recipes by search_text' do
     visit recipes_path
     fill_in 'search_text', with: 'spaghetti'
-    click_button 'Search'
-
+    within '#recipes' do
+      click_button 'Search'
+    end
     expect(page).to have_content(recipe1.title)
     expect(page).not_to have_content(recipe2.title)
   end
 
-  it 'only matches words starting with the search_text' do
+  it 'only matches words STARTING with the search_text' do
     visit recipes_path
     fill_in 'search_text', with: 'sal' # both recipes have salt
-    click_button 'Search'
-
+    within '#recipes' do
+      click_button 'Search'
+    end
     expect(page).to have_content(recipe1.title)
     expect(page).to have_content(recipe2.title)
 
     fill_in 'search_text', with: 'ian' # both recipes have "ian" in "Italian" and "Indian"
-    click_button 'Search'
-
+    within '#recipes' do
+      click_button 'Search'
+    end
     expect(page).not_to have_content(recipe1.title)
+    expect(page).not_to have_content(recipe2.title)
+  end
+
+  it 'searches recipes by search_text (case insensitive)' do
+    visit recipes_path
+    fill_in 'search_text', with: 'SPAGHETTI'
+    within '#recipes' do
+      click_button 'Search'
+    end
+    expect(page).to have_content(recipe1.title)
     expect(page).not_to have_content(recipe2.title)
   end
 
   it 'searches recipes by search_ingredient_ids' do
     visit recipes_path
     select ingredient4.name, from: 'search_ingredient_ids'
-    click_button 'Search'
-
+    within '#recipes' do
+      click_button 'Search'
+    end
     expect(page).not_to have_content(recipe1.title)
     expect(page).to have_content(recipe2.title)
 
     select ingredient0.name, from: 'search_ingredient_ids'
-    click_button 'Search'
-
+    within '#recipes' do
+      click_button 'Search'
+    end
     expect(page).to have_content(recipe1.title)
     expect(page).to have_content(recipe2.title)
   end
